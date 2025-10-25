9b0991e5f13dcbf72c31d4a525ae66f68621f594
VictorMS-200 <victorm20005@gmail.com>
2025-10-24 23:49:35 -0300

    Adicionando Produto e conexão ao banco de dados mysql

---
+++ Context/AsciiDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Projeto_Ascii.Models;
+
+namespace Projeto_Ascii.Context;
+
+public class AsciiDbContext : DbContext
+{
+    public AsciiDbContext(DbContextOptions options) : base(options) {}
+    
+    public DbSet<Produto> Produtos { get; set; }
+}
---
+++ Migrations/20251025003410_Primeira-Migracao.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Projeto_Ascii.Migrations
+{
+    /// <inheritdoc />
+    public partial class PrimeiraMigracao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    Nome = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Preco = table.Column<decimal>(type: "decimal(65,30)", nullable: false),
+                    Categoria = table.Column<string>(type: "varchar(20)", maxLength: 20, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
